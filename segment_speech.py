e10ae2579ec4327ef46c4da60034c4db68570d0c
Taha Husain <tahamh77@gmail.com>
2022-11-07 17:10:03 -0500

    Montreal demo

--- segment_speech.py
+++
@@ -1,8 +0,0 @@
-import pykaldi
-
-def main():
-    git@github.com:kaldi-asr/kaldi.git
-
-if __name__ == '__main__':
-    main()
-
---
+++ vowel_plotter/segment_speech.py
@@ -0,0 +1,9 @@
+from .get_formants import get_formants
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
