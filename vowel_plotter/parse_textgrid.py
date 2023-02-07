113a342582bc856f90611650e3f19bc91e9949be
Taha Husain <tahamh77@gmail.com>
2023-02-06 19:36:28 -0500

    textgrid parsing

---
+++ vowel_plotter/parse_textgrid.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from collections import defaultdict
+
+from praatio import textgrid
+
+from get_formants import get_formants
+
+
+DEFAULT_VOWELS = {'IY1', 'IH1', 'EY1', 'EH1', 'AE1', 'AA1', 'OW1', 'AH1', 'UH1', 'UW1'}
+
+def parse_textgrid(path: str, vowel_set: set = None):
+    vowel_times = defaultdict(list)
+    if not vowel_set:
+        vowel_set = DEFAULT_VOWELS
+    tg = textgrid.openTextgrid(Path(path), False)
+    vowel_tier = tg.tierDict['phones']
+    for start, end, phone in vowel_tier.entryList:
+        if phone in vowel_set:
+            vowel_times[phone].append((start, end))
+
+    print(vowel_times)
+
+if __name__ == '__main__':
+    path = 'data/corpus_aligned/speaker1.TextGrid'
+    parse_textgrid(path)
--- vowel_plotter/segment_speech.py
+++
@@ -1,9 +0,0 @@
-from .get_formants import get_formants
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
