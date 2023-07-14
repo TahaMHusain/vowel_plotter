fe05c70f82056467fe9ca2448b703f2a55cb378a
Taha Husain <tahamh77@gmail.com>
2023-07-14 19:08:14 -0400

    fixed postgres permissions issue?

--- vowel_plotter/align.py
+++ vowel_plotter/align.py
@@ -1,5 +1,4 @@
 from montreal_forced_aligner.alignment.pretrained import PretrainedAligner
-from montreal_forced_aligner.command_line.utils import check_databases, cleanup_databases
 
 
 def align(corpus_path: str, dictionary_path: str, acoustic_model_path: str, output_directory: str):
@@ -15,9 +14,8 @@ def align(corpus_path: str, dictionary_path: str, acoustic_model_path: str, outp
         corpus_directory=corpus_path,
         dictionary_path=dictionary_path,
         acoustic_model_path=acoustic_model_path)
+    
 
-    check_databases()
     aligner.align()
     aligner.export_files(output_directory, output_format='long_textgrid')
     aligner.cleanup()
-    cleanup_databases()
